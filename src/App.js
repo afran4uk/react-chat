417710f44d0c2cf97a2758afe888c1fc8e5949db
Франчук Антон <franchukanton182@gmail.com>
2021-10-17 13:41:32 +0300

    add block and button conmponents

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, {Component} from "react";
+import { Button, Block } from "./components";
 
 class App extends Component {
     render() {
-        return <div className="wrapper">Hello world!</div>;
+        return (
+            <div className="wrapper">
+                <Block>
+                    <Button type="primary" size="large">
+                        This is Button!
+                    </Button>
+                </Block>
+            </div>
+        )
     }
 }
 
---
+++ src/components/Block/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './Block.scss';
+
+const Block = ({ children }) => (
+    <div className="block">{ children }</div>
+);
+
+
+export default Block;
