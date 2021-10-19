246c62b35148033efe6b4f2ef3672cc8738052d4
Франчук Антон <franchukanton182@gmail.com>
2021-10-19 23:49:58 +0300

    save final auth form

--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -1,66 +1,69 @@
 import React from 'react';
-import { Form, Input, Checkbox } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import { Button, Block } from "../../components"
+import {Form, Input, Checkbox} from 'antd';
+import {UserOutlined, LockOutlined} from '@ant-design/icons';
+import {Button, Block} from "../../components"
 
 import "./Auth.scss"
 
-const Auth = () => {
-    const onFinish = (values: any) => {
+class Auth extends React.Component {
+    onFinish = (values: any) => {
         console.log('Received values of form: ', values);
     };
 
-    return (
-        <section className="auth">
-            <div>
-                <div className="auth__top">
-                    <h2>Войти в аккаунт</h2>
-                    <p>Пожалуйста, войдите в свой аккаунт</p>
-                </div>
-                <Block>
-                    <Form
-                        name="normal_login"
-                        className="login-form"
-                        initialValues={{ remember: true }}
-                        onFinish={onFinish}
-                    >
-                        <Form.Item
-                            name="username"
-                            rules={[{ required: true, message: 'Please input your Username!' }]}
+    render() {
+        return (
+            <section className="auth">
+                <div>
+                    <div className="auth__top">
+                        <h2>Войти в аккаунт</h2>
+                        <p>Пожалуйста, войдите в свой аккаунт</p>
+                    </div>
+                    <Block>
+                        <Form
+                            name="normal_login"
+                            className="login-form"
+                            onFinish={this.onFinish}
                         >
-                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-                        </Form.Item>
-                        <Form.Item
-                            name="password"
-                            rules={[{ required: true, message: 'Please input your Password!' }]}
-                        >
-                            <Input
-                                prefix={<LockOutlined className="site-form-item-icon" />}
-                                type="password"
-                                placeholder="Password"
-                            />
-                        </Form.Item>
-                        <Form.Item>
-                            <Form.Item name="remember" valuePropName="checked" noStyle>
-                                <Checkbox>Remember me</Checkbox>
+                            <Form.Item
+                                name="username"
+                                rules={[{required: true, message: 'Please input your Username!'}]}
+                                hasFeedback
+                                validate-status="success"
+                            >
+                                <Input
+                                    size="large"
+                                    prefix={<UserOutlined className="site-form-item-icon"/>}
+                                    placeholder="Username"
+                                />
+                            </Form.Item>
+                            <Form.Item
+                                name="password"
+                                rules={[{required: true, message: 'Please input your Password!'}]}
+                            >
+                                <Input
+                                    size="large"
+                                    prefix={<LockOutlined className="site-form-item-icon"/>}
+                                    type="password"
+                                    placeholder="Password"
+                                />
                             </Form.Item>
 
-                            <a className="login-form-forgot" href="">
-                                Forgot password
-                            </a>
-                        </Form.Item>
+                            <Form.Item>
+                                <Button
+                                    type="primary"
+                                    size="large">
+                                    Войти в аккаунт
+                                </Button>
+                            </Form.Item>
 
-                        <Form.Item>
-                            <Button type="primary" htmlType="submit" className="login-form-button">
-                                Log in
-                            </Button>
-                            Or <a href="">register now!</a>
-                        </Form.Item>
-                    </Form>
-                </Block>
-            </div>
-        </section>
-    )
+                            <a className="auth__register-link" href="">Зарегистрироваться</a>
+
+                        </Form>
+                    </Block>
+                </div>
+            </section>
+        )
+    }
 }
 
-export { Auth };
+export {Auth};
