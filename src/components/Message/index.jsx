f830e0ef56171fa7441449ff4138ecb8cfb94979
Франчук Антон <franchukanton182@gmail.com>
2021-10-28 23:12:33 +0300

    ui layout

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -4,18 +4,36 @@ import classNames from 'classnames';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import ruLocale from 'date-fns/locale/ru';
 
+import readedSvg from '~/assets/img/readed.svg';
+import noreadedSvg from '~/assets/img/noreaded.svg';
+
 import './Message.scss';
 
-export const Message = ({ avatar, user, text, date, isMe }) => (
+export const Message = ({ avatar, user, text, date, isMe, isReaded, attachments }) => (
   <div className={classNames('message', { 'message--isMe': isMe })}>
-    <div className="message__avatar">
-      <img src={avatar} alt={`Avatar ${user.fullName}`} />
-    </div>
     <div className="message__content">
-      <div className="message__bubble">
-        <p className="message__text">{text}</p>
+      {isMe && isReaded ? (
+        <img className="message__icon-readed" src={readedSvg} alt="Readed icon" />
+      ) : (
+        <img className="message__icon-readed message__icon-readed--no" src={noreadedSvg} alt="NoReaded Icon" />
+      )}
+      <div className="message__avatar">
+        <img src={avatar} alt={`Avatar ${user.fullName}`} />
       </div>
-      <span className="message__date">{formatDistanceToNow(date, { locale: ruLocale })}</span>
+      <div className="message__info">
+        <div className="message__bubble">
+          <p className="message__text">{text}</p>
+        </div>
+        <div className="message__attachments">
+          {attachments &&
+          attachments.map((item) => (
+            <div className="message__attachments-item">
+              <img src={item.url} alt={item.filename} />
+            </div>
+          ))}
+        </div>
+        <span className="message__date">{formatDistanceToNow(date, { locale: ruLocale })}</span>
+      </div>
     </div>
   </div>
 );
@@ -29,4 +47,5 @@ Message.propTypes = {
   text: PropTypes.string,
   user: PropTypes.object,
   date: PropTypes.string,
+  attachments: PropTypes.array,
 };
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,6 +9,21 @@ export const Home = () => (
       avatar="https://cdn-icons-png.flaticon.com/512/147/147144.png"
       text="A"
       date={new Date()}
+      isReaded={true}
+      attachments={[
+        {
+          filename: 'image.jpg',
+          url: 'https://source.unsplash.com/100x100/?random1&nature,water',
+        },
+        {
+          filename: 'image.jpg',
+          url: 'https://source.unsplash.com/100x100/?random2&nature,water',
+        },
+        {
+          filename: 'image.jpg',
+          url: 'https://source.unsplash.com/100x100/?random3&nature,water',
+        },
+      ]}
     />
 
     <Message
@@ -16,6 +31,8 @@ export const Home = () => (
       text="Test stgsg"
       date={new Date()}
       isMe={true}
+      isReaded={false}
+
     />
   </section>
 );
