2177e887852ab2291f9eb0d872c4b7520f8d02f1
Франчук Антон <franchukanton182@gmail.com>
2021-11-21 01:52:16 +0300

    add formik (yes)

--- src/modules/RegisterForm/components/RegisterForm.jsx
+++ src/modules/RegisterForm/components/RegisterForm.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Block, Button } from '~/components';
 import { Form, Input } from 'antd';
+import { Formik } from 'formik';
 import { ClockCircleTwoTone } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 
 const success = false;
 
-export const RegisterForm = (props) => {
-  const { values, touched, errors, handleChange, handleBlur, handleSubmit } = props;
-  console.log(props)
+export const RegisterForm = () => {
   return (
     <div>
       <div className="auth__top">
@@ -17,46 +16,126 @@ export const RegisterForm = (props) => {
       </div>
       <Block>
         {!success ? (
-          <Form className="login-form" onSubmit={handleSubmit}>
-            <Form.Item validateStatus={errors.email && touched.email} hasFeedback>
-              <Input
-                id='email'
-                name='email'
-                size="large"
-                type="mail"
-                placeholder="E-mail"
-                value={values.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-            </Form.Item>
-            <Form.Item name="username">
-              <Input size="large" placeholder="Username" value={values.username} />
-            </Form.Item>
-            <Form.Item name="password">
-              <Input size="large" type="password" placeholder="Password" value={values.password} />
-            </Form.Item>
-            <Form.Item name="repeat_password">
-              <Input size="large" type="password" placeholder="Repeat password" value={values.repeat_password} />
-            </Form.Item>
+          <Formik
+            initialValues={{ email: '', username: '', password: '', repeat_password: '' }}
+            validate={(values) => {
+              const errors = {};
 
-            <Form.Item>
-              <Button type="primary" size="large">
-                Зарегистрироваться
-              </Button>
-            </Form.Item>
+              if (!values.email) {
+                errors.email = 'Required';
+              } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+              }
 
-            <Link className="auth__register-link" to="/">
-              Войти в аккаунт
-            </Link>
-          </Form>
+              if (!values.password) {
+                errors.password = 'Required';
+              }
+
+              if (!values.username) {
+                errors.username = 'Required';
+              }
+
+              if (!values.repeat_password) {
+                errors.repeat_password = 'Required';
+              }
+
+              if (values.repeat_password !== values.password) {
+                errors.repeat_password = 'Пароли должны совпадать';
+              }
+
+              return errors;
+            }}
+            onSubmit={(values, actions) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+
+                actions.setSubmitting(false);
+              }, 1000);
+            }}
+          >
+            {(props) => (
+              <Form className="login-form" onSubmit={props.handleSubmit}>
+                <Form.Item
+                  validateStatus={!props.touched.email ? '' : props.errors.email ? 'error' : 'success'}
+                  hasFeedback
+                >
+                  <Input
+                    id="email"
+                    name="email"
+                    size="large"
+                    type="mail"
+                    placeholder="E-mail"
+                    value={props.values.email}
+                    onChange={props.handleChange}
+                    onBlur={props.handleBlur}
+                  />
+                </Form.Item>
+                <Form.Item
+                  validateStatus={!props.touched.username ? '' : props.errors.username ? 'error' : 'success'}
+                  hasFeedback
+                >
+                  <Input
+                    id="userName"
+                    name="username"
+                    size="large"
+                    placeholder="Username"
+                    value={props.values.username}
+                    onChange={props.handleChange}
+                    onBlur={props.handleBlur}
+                  />
+                </Form.Item>
+                <Form.Item
+                  validateStatus={!props.touched.password ? '' : props.errors.password ? 'error' : 'success'}
+                  hasFeedback
+                >
+                  <Input
+                    id="password"
+                    name="password"
+                    size="large"
+                    type="password"
+                    placeholder="Password"
+                    value={props.values.password}
+                    onChange={props.handleChange}
+                    onBlur={props.handleBlur}
+                  />
+                </Form.Item>
+                <Form.Item
+                  validateStatus={
+                    !props.touched.repeat_password ? '' : props.errors.repeat_password ? 'error' : 'success'
+                  }
+                  hasFeedback
+                >
+                  <Input
+                    id="repeat_password"
+                    name="repeat_password"
+                    size="large"
+                    type="password"
+                    placeholder="Repeat password"
+                    value={props.values.repeat_password}
+                    onChange={props.handleChange}
+                    onBlur={props.handleBlur}
+                  />
+                </Form.Item>
+
+                <Form.Item>
+                  <Button type="primary" size="large">
+                    Зарегистрироваться
+                  </Button>
+                </Form.Item>
+
+                <Link className="auth__register-link" to="/">
+                  Войти в аккаунт
+                </Link>
+              </Form>
+            )}
+          </Formik>
         ) : (
           <div className="auth__success-block">
             <div>
               <ClockCircleTwoTone style={{ size: '50px' }} />
             </div>
-            <h3>Подверлите свой аккаунт</h3>
-            <p>На вашу почту отправлено письмецо</p>
+            <h3>Подтвердите свой аккаунт</h3>
+            <p>На вашу почту отправлено письмо</p>
           </div>
         )}
       </Block>
--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { LoginForm } from '~/modules/LoginForm';
-import { RegisterFormik } from '~/modules/RegisterForm';
+import { RegisterForm } from '~/modules/RegisterForm';
 import { Route } from 'react-router';
 
 import './Auth.scss';
@@ -9,7 +9,7 @@ export const Auth = () => (
   <section className="auth">
     <div className="auth__content">
       <Route exact path={['/', '/login']} component={LoginForm} />
-      <Route exact path="/register" component={RegisterFormik} />
+      <Route exact path="/register" component={RegisterForm} />
     </div>
   </section>
 );
